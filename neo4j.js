0681b4f1b221eba199a5c953a820ade85a78220e
Daniel Garcia <danielgm2111@gmail.com>
2023-11-23 01:28:39 -0300

    remove dot env import

--- neo4j.js
+++ neo4j.js
@@ -1,12 +1,10 @@
-const e = require( 'express' )
-var neo4j = require('neo4j-driver')
-require('dotenv').config()
+const e = require('express');
+var neo4j = require('neo4j-driver');
 
-const URI = process.env.NEO4J_HOST
-const USER = process.env.NEO4J_USER
-const PASSWORD = process.env.NEO4J_PASS
+const URI = process.env.NEO4J_HOST;
+const USER = process.env.NEO4J_USER;
+const PASSWORD = process.env.NEO4J_PASS;
 
-const driver = neo4j.driver(URI,
-neo4j.auth.basic(USER, PASSWORD));
+const driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
 
-exports.driver = driver
+exports.driver = driver;
