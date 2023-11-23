4c137f3a51eabaef16b3c6f62390c58f6b269b5e
DanielGM <danielgm2111@gmail.com>
2023-11-23 00:02:24 -0300

    Fix endpoints (#1)
    * fix-endpoints
    
    * some fixes
    
    ---------
    
    Co-authored-by: Daniela de la Sierra <daniela.delasierra.9@gmail.com>

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,7 +1,7 @@
 const Joi = require('@hapi/joi');
 
 const personaSchema = Joi.object({
-    ci: Joi.number().integer().min(100000).max(99999999).required(),
+    ci: Joi.string().min(7).max(8).required(),
     nombre: Joi.string().min(3).max(30).required(),
     apellido: Joi.string().min(3).max(30).required(),
     edad: Joi.number().integer().min(0).max(120).required()
@@ -11,8 +11,8 @@ const personaSchema = Joi.object({
     departamento: Joi.string().required(),
     localidad: Joi.string().required(),
     calle: Joi.string().required(),
-    nro: Joi.number().integer().required(),
-    apartamento: Joi.string(),
+    nro: Joi.number().integer().min(1).required(),
+    apartamento: Joi.number().integer().min(1),
     padron: Joi.string(),
     ruta: Joi.string(),
     km: Joi.string(),
